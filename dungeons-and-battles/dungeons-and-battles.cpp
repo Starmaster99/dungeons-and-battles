519b15e78d289156eb533b761fe786147aeef8eb
Vladyslav Kuznietsov <starforce882@gmail.com>
2024-01-09 21:36:42 +0100

    Move classes to separate files

--- dungeons-and-battles/dungeons-and-battles.cpp
+++ dungeons-and-battles/dungeons-and-battles.cpp
@@ -12,81 +12,10 @@
 #include <iostream>
 #include <ctime>
 
-/// <summary>
-/// Generate a random number.
-/// </summary>
-/// <param name="min">Starting value.</param>
-/// <param name="max">Ending value.</param>
-/// <returns>A random integer between `min` and `max` numbers</returns>
-int generateRandomNumber(int min, int max) {
-    return min + std::rand() % (max - min + 1);
-}
-
-// Prototype interface class for Enemy. That's the root of all Enemy objects here. Every Enemy object inherits
-// props of that class
-class IEnemy {
-public:
-    // Virtual destructor. Used to safely delete objects after the game is completed. The "virtual" part here
-    // means that destructor is abstract, just like everything in this class, providing a good level of polymorphism
-    virtual ~IEnemy() {}
-    // a sort of "required" function. For now, a dummy function. 
-    // Must be overriden in children classes, otherwise wouldn't work
-    virtual void displayInfo() = 0;
-
-
-    std::string type;   // Determines the type of the enemy, i.e. different properties for different Enemy types
-    int health;         // Determines the amount of hitpoints needed to kill an Enemy
-    int damage;         // Determines the damage the Enemy deals to Player
-    int speed;          // Determines how fast an Enemy moves, meaning who's move is first
-    std::string name;   // Enemy's name
-};
-
-// A "child" class of IEnemy interface class. IEnemy here is called a "parent" class. That's all about inheritance
-class Spider : public IEnemy {
-public:
-    // Here, we initialize that class when EnemyFactory design pattern function is called
-    Spider() {
-        type = "spider";
-        health = generateRandomNumber(1, 5);
-        damage = generateRandomNumber(1, 5);
-        speed = generateRandomNumber(1, 10);
-        name = "Silky Spider";
-    }
-    // Here, we override that virtual method. Polymorphism in all it's glory
-    void displayInfo() override {
-        std::cout << "Enemy: "   << name
-                  << "\nType: "  << type
-                  << "\nHP: "    << health
-                  << "\nDMG: "   << damage
-                  << "\nSpeed: " << speed;
-    };
-    // Private access level means that no other method in all other classes can modify these variables.
-    // Encapsulation is responsible for it.
-private:
-    std::string type;
-    int health;
-    int damage;
-    int speed;
-    std::string name;
-};
-
-class Zombie : public IEnemy {
-public:
-    Zombie() {
-        type = "zombie";
-        health = generateRandomNumber(1, 7);
-        damage = generateRandomNumber(1, 3);
-        speed = generateRandomNumber(1, 5);
-        name = "Hungry Zombie";
-    }
-    void displayInfo() override {
-        std::cout << "Enemy: "   << name
-                  << "\nType: "  << type
-                  << "\nHP: "    << health
-                  << "\nDMG: "   << damage
-                  << "\nSpeed: " << speed;
-    };
-};
+#include "IEnemy.h"
+#include "Zombie.h"
+#include "Spider.h"
+#include "Game.h"
 
 IEnemy* EnemyFactory(std::string type) {
     if (type == "zombie") {
@@ -95,51 +24,7 @@ IEnemy* EnemyFactory(std::string type) {
     else if (type == "spider") {
         return new Spider;
     }
- }
-
-/// <summary>
-/// A meta class used for different game-related things.
-/// </summary>
-class Game {
-public:
-    /// <summary>
-    /// Prints a separation line for a better view.
-    /// </summary>
-    /// <param name="startline">`1` if the line starts with a newline character.</param>
-    /// <param name="endline">`1` if the line ends with a newline character.</param>
-    void printSep(bool startline, bool endline) {
-        if (startline) {
-            std::cout << std::endl;
-        }
-        std::cout << "=============================================";
-        if (endline) {
-            std::cout << std::endl;
-        }
-    }
-
-    void start() {
-
-    }
-
-    /// <summary>
-    /// Prints game's logo.
-    /// </summary>
-    void printLogo() {
-        printSep(0, 0);
-        // Here we use raw output because of "\" characters.
-        std::cout << R"(
-            ____        ____  
-            |  _ \ _ __ | __ ) 
-            | | | | '_ \|  _ \ 
-            | |_| | | | | |_) |
-            |____/|_| |_|____/
-
-            Dungeons and Battles
-)";
-        printSep(0, 0);
-        std::cout << std::endl;
-    }
-};
+}
 
 int main()
 {
